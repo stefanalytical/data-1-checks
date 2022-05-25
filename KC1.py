e6842f91b939491dd0765b89c18e9f7df124460a
Stefan Vuleta <vuleta99@gmail.com>
2022-05-25 04:53:20 -0400

    Isolated rows and columns

--- KC1.py
+++ KC1.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 import json
 
-# Get data from web API and normalize into Pandas Dataframe
+# Get data from web API and normalize into Pandas data frame
 r = requests.get('https://www.balldontlie.io/api/v1/players')
 x = r.json()
 df = pd.json_normalize(x, 'data')
@@ -19,5 +19,14 @@ print('Number of Players With Unreported Heights (NaN): ' + str(teams))
 
 # Perform query to only display Grizzlies
 query = df[df['team.name'] == 'Grizzlies']
-print('Below is a queried subset showing only Grizzlies:')
-print(query)
+print(f'Below is a queried subset showing only Grizzlies:\n{query}\n')
+
+
+# Second and third columns of my data frame
+dfcol = df.iloc[:,[1,2]]
+print(f'Below are the 2nd and 3rd Columns:\n{dfcol}\n')
+
+
+# First four rows of my data frame
+dfrow = df.iloc[0:4]
+print(f'Below are the first 4 rows:\n{dfrow}')
