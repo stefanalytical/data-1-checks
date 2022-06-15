cd29d7d2a01eb438fcf493619fe80a1cd5405408
Stefan <95545478+stefanalytical@users.noreply.github.com>
2022-06-15 01:57:51 -0400

    Cleaned up data and added comments

--- data_2_kc_2.py
+++ data_2_kc_2.py
@@ -7,21 +7,35 @@ x = r.json()
 df = pd.json_normalize(x, 'data')
 print(df)
 
+# See all 14 columns and determine which can be dropped.
 print(df.columns)
 
-df.drop(['id', 'height_inches', 'height_feet', 'team.id', 'weight_pounds'], axis = 1, inplace=True)
-print(df)
+# Drop columns that are not needed.
+df.drop(['id', 'height_inches', 'height_feet', 'team.id', 'team.city', 'team.name'], axis = 1, inplace = True)
 
+# Rename and change column names to snake case.
 fixed_columns = {
     'team.abbreviation':'team_abbreviation',
-    'team.city':'city',
     'team.conference':'conference',
     'team.division':'division',
     'team.full_name':'team_full_name',
-    'team.name':'team_name',
-    
+
 }
 
-df.rename(columns=fixed_columns,inplace=True)
-print(df.columns)
+df.rename(columns = fixed_columns, inplace = True)
+print(df)
+
+# Count and display the number of cells thate are NaN.
+nan = df.isnull().sum().sum()
+print('Number of NaNs: ' + str(nan))
+
+# Replace all NaN values with 0.
+df['weight_pounds'] = df['weight_pounds'].fillna(0)
+print(df)
+
+# Count and display the number of cells thate are NaN again. Will display zero.
+nan = df.isnull().sum().sum()
+print('Number of NaNs: ' + str(nan))
+
+
 
